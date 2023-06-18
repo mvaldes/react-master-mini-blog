40fdbf14addafea746ff0508e5a9e32fb31eb96a
Bryan BERGER <contact@weberger.net>
2023-06-18 18:19:44 +0200

    add 4th exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import Button from './components/Button';
 import Article from './components/Article';
 import Tabs from './components/Tabs';
 
@@ -34,30 +32,10 @@ const tabs = [
 ];
 
 const App = () => {
-  const [ isArticleDisplayed, setIsArticleDisplayed ] = useState(false);
-
-  const handleToggleArticle = () => {
-	setIsArticleDisplayed(!isArticleDisplayed);
-  };
 
   return (
 	<>
 		<h1>Mini Blog</h1>
-		<Button
-			variant={ isArticleDisplayed ? 'danger' : 'primary' }
-			type="button"
-			onClick={ handleToggleArticle }
-		>
-			{ isArticleDisplayed ? 'Hide' : 'Show' } article
-		</Button>
-		{
-			isArticleDisplayed &&
-			<Article title="My Article">
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium molestiae obcaecati, velit perferendis libero dolore inventore dolorum vero, voluptatum totam officiis a? Doloribus blanditiis rerum consectetur neque autem? Sunt, quo.
-				</p>
-			</Article>
-		}
 		<Tabs defaultActiveTab={ 2 } tabs={ tabs } />
 	</>
   );
