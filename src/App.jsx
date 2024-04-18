9ad5c8f20de7f42d8cf6e0129ff31eb078cfb046
Mauro Valdes <mvrodrigo40@hotmail.com>
2024-04-18 11:22:16 -0400

    changement nom de variable significatif

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from 'react';
-import { render } from 'react-dom';
 
 const App = () => {
-	const [article, setArticle] = useState(false);
+	const [isArticle, setArticle] = useState(false);
 
 	const handleClick = () => {
-		setArticle(!article)
+		setArticle(!isArticle);
 	}
 
 	return (
 		<>
 			<h1>Mini blogue</h1>
-			<button className={`btn ${article ? "btn--success":"btn--primary"}`} onClick={handleClick}>{article ? "Hide article" : "Show article"}</button>
-			{article && (
+			<button className={`btn ${isArticle ? "btn--success":"btn--primary"}`} onClick={handleClick}>{isArticle ? "Hide article" : "Show article"}</button>
+			{isArticle && (
 					<div className="btn--success">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, possimus similique in corrupti vel deleniti porro error totam commodi rem nobis officiis! Corrupti ullam qui ipsam neque commodi molestiae laborum.</div>
 			)}
 		</>
