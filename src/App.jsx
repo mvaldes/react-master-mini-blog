76536729f63c7931731398b494035b0ce497fad1
Mauro Valdes <mvrodrigo40@hotmail.com>
2024-04-18 11:19:09 -0400

    ajout du titre, bouton et text

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+import { render } from 'react-dom';
+
 const App = () => {
-	// Write the code of your component here
+	const [article, setArticle] = useState(false);
+
+	const handleClick = () => {
+		setArticle(!article)
+	}
+
+	return (
+		<>
+			<h1>Mini blogue</h1>
+			<button className={`btn ${article ? "btn--success":"btn--primary"}`} onClick={handleClick}>{article ? "Hide article" : "Show article"}</button>
+			{article && (
+					<div className="btn--success">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, possimus similique in corrupti vel deleniti porro error totam commodi rem nobis officiis! Corrupti ullam qui ipsam neque commodi molestiae laborum.</div>
+			)}
+		</>
+	);
 };
 
 export default App;
